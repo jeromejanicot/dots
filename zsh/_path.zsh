fa8dc073ad280125f301dcbacbc6c6327596bcf4
Jerome Janicot <hello@jeromejanicot.com>
2025-06-22 18:44:03 +0200

    zsh add

--- zsh/_path.zsh
+++ zsh/_path.zsh
@@ -1,14 +1,6 @@
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+#
+export PATH="$PATH:$HOME/.local/bin";
 
-
-# Go development
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/go
-export PATH="$PATH:$GOPATH/bin"
-
-# Rust development 
-export PATH="$PATH:$HOME/.cargo/bin"
-
-# Bun development
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
+# proto
+export PROTO_HOME="$HOME/.proto";
+export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH";
